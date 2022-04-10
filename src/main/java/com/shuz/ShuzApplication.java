37c9bc9a3436e3a596fb70c2e268a5b731c49983
김윤형 <idol981112@gmail.com>
2022-04-10 23:02:58 +0900

    intellij 첫 깃허브 연동

---
+++ src/main/java/com/shuz/ShuzApplication.java
@@ -0,0 +1,13 @@
+package com.shuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuzApplication.class, args);
+	}
+
+}
